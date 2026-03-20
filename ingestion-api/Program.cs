9a9c1d192d85c1360becde171947d17935d291c8
Kalaiyarasi26 <kalaiinba123@gmail.com>
2026-03-20 16:58:06 -0500

    Added POST method to insert values to DB

--- ingestion-api/Program.cs
+++ ingestion-api/Program.cs
@@ -2,6 +2,10 @@ using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
+using System.Net;
+using System.IO;
+using CsvHelper;
+using System.Globalization;
 using Microsoft.Data.Sqlite;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +13,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddAntiforgery(options =>
+{
+    options.SuppressXFrameOptionsHeader = true;
+});
 
 var app = builder.Build();
 
@@ -76,6 +84,97 @@ app.MapGet("/products", () =>
     }
     Console.WriteLine("Database connection closed!");
     return  products;
+});
+
+
+app.MapPost("/products/upload", async (HttpRequest request) =>
+{
+    // Handle file upload logic here
+    var form = await request.ReadFormAsync();
+    string dbpath = "/Users/kalai/Documents/Projects/ingestion-api/ingestion-api/ingestion.db";
+    var files = form.Files;
+    Console.WriteLine(files);
+    
+    if (files.Count  == 0)
+    {
+        return Results.BadRequest("No file uploaded");
+    }
+      var firstFile= files[0];
+    if (firstFile.Length == 0)
+    {
+        return Results.BadRequest("File is empty");
+    }
+   
+    var stream =  firstFile.OpenReadStream();
+    var reader = new StreamReader(stream);
+    var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+    csv.Read();
+    var header = csv.ReadHeader();
+    var rowsProcessed = 0;
+    string connectionString = "Data Source=" + dbpath;
+        
+        using (var connection = new SqliteConnection(connectionString))
+    {
+        try
+        {
+            connection.Open();
+            while (csv.Read()){
+
+                rowsProcessed ++;
+                var ProductID= csv.GetField("product_id");
+                var ProductName= csv.GetField("product_name");
+                var DiscountedPrice= csv.GetField("discounted_price");
+                DiscountedPrice = DiscountedPrice.Replace(",", "");
+                var ActualPrice= csv.GetField("actual_price");
+                ActualPrice = ActualPrice.Replace(",", "");
+                var Rating= csv.GetField("rating");
+                var RatingCount= csv.GetField("rating_count");
+                RatingCount = RatingCount.Replace(",", "");
+                Console.WriteLine($"ProductID: {ProductID}, ProductName: {ProductName}, DiscountedPrice: {DiscountedPrice}, ActualPrice: {ActualPrice}, Rating: {Rating}, RatingCount: {RatingCount}");
+
+                // Convert numeric values safely
+                string productIdValue = string.IsNullOrWhiteSpace(ProductID)
+                    ? "Unknown"
+                    : ProductID;
+
+                string productNameValue = string.IsNullOrWhiteSpace(ProductName)
+                    ? "Unknown"
+                    : ProductName;
+                double discountedPriceValue;
+                double.TryParse(DiscountedPrice, out discountedPriceValue);
+
+                double actualPriceValue;
+                double.TryParse(ActualPrice, out actualPriceValue);
+
+                double ratingValue;
+                double.TryParse(Rating, out ratingValue);
+
+                int ratingCountValue;
+                int.TryParse(RatingCount, out ratingCountValue);
+
+                 var insertSql = @"INSERT INTO products_raw
+                (product_id, product_name, discounted_price, actual_price, rating, rating_count)
+                VALUES
+                (@product_id, @product_name, @discounted_price, @actual_price, @rating, @rating_count)";
+                var command = new SqliteCommand(insertSql, connection);
+
+                command.Parameters.AddWithValue("@product_id", productIdValue);
+                command.Parameters.AddWithValue("@product_name", productNameValue);
+                command.Parameters.AddWithValue("@discounted_price", discountedPriceValue);
+                command.Parameters.AddWithValue("@actual_price", actualPriceValue);
+                command.Parameters.AddWithValue("@rating", ratingValue);
+                command.Parameters.AddWithValue("@rating_count", ratingCountValue);
+
+                command.ExecuteNonQuery();
+                            
+            }
+        }
+         catch(SqliteException ex)
+        {
+            Console.WriteLine("Error connecting to database: " + ex.Message);
+        }
+    }
+   return Results.Ok(new { rowsProcessed });
 });
 
 app.Run();
